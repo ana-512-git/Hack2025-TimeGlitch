807eca557935918a6611dccbe7fa6ed1c4f6f817
dannarcis24 <dannarcis.costinel@gmail.com>
2025-04-05 03:44:26 +0300

    left + right moves

--- TimeGlitch/Assets/Scripts/PlayerMovement.cs
+++ TimeGlitch/Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-    private bool idle = true, walk_back = false, walk_front = false;
+    private bool idle = true, walk_back = false, walk_front = false, walk_left = false, walk_right = false;
     private Animator animation;
 
     void Awake() {
@@ -19,24 +19,51 @@ public class PlayerMovement : MonoBehaviour
         if(horizontal == 0 && vertical == 0 && !idle) {
             animation.ResetTrigger("isWalkingBack");
             animation.ResetTrigger("isWalkingFront");
+            animation.ResetTrigger("isWalkingLeft");
+            animation.ResetTrigger("isWalkingRight");
             animation.SetTrigger("isIdle");
             idle = true;
-            walk_back = walk_front = false;
+            walk_back = walk_front = walk_left = walk_right = false;
         }
-
+        
+        // walk up or down
         if(vertical > 0 && !walk_back) {
             animation.ResetTrigger("isWalkingFront");
             animation.ResetTrigger("isIdle");
+            animation.ResetTrigger("isWalkingLeft");
+            animation.ResetTrigger("isWalkingRight");
             animation.SetTrigger("isWalkingBack");
             walk_back = true;
-            idle = walk_front = false;
+            idle = walk_front = walk_left = walk_right = false;
         }
         else    if(vertical < 0 && !walk_front) {
             animation.ResetTrigger("isWalkingBack");
             animation.ResetTrigger("isIdle");
+            animation.ResetTrigger("isWalkingLeft");
+            animation.ResetTrigger("isWalkingRight");
             animation.SetTrigger("isWalkingFront");
             walk_front = true;
-            idle = walk_back = false;
+            idle = walk_back = walk_left = walk_right = false;
+        }
+
+        // walk left or right
+        if(horizontal > 0 && !walk_right) {
+            animation.ResetTrigger("isIdle");
+            animation.ResetTrigger("isWalkingLeft");
+            animation.ResetTrigger("isWalkingFront");
+            animation.ResetTrigger("isWalkingBack");
+            animation.SetTrigger("isWalkingRight");
+            walk_right = true;
+            idle = walk_back = walk_left = walk_front = false;
+        }
+        else    if(horizontal < 0 && !walk_left) {
+            animation.ResetTrigger("isIdle");
+            animation.ResetTrigger("isWalkingRight");
+            animation.ResetTrigger("isWalkingFront");
+            animation.ResetTrigger("isWalkingBack");
+            animation.SetTrigger("isWalkingLeft");
+            walk_left = true;
+            idle = walk_back = walk_right = walk_front = false;
         }
     }
 }
